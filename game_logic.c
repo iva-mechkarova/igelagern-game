1bffd98116bb39d30c42c2f587078e674ed8174f
iva-mechkarova <i.mekarova@live.ie>
2019-04-01 13:40:15 +0100

    place_tokens

--- game_logic.c
+++ game_logic.c
@@ -76,6 +76,27 @@ void printLine(){
  */
 void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers){
     // TO BE IMPLEMENTED
+    int minNumOfTokens = 0;
+    int selectedSquare = 0;
+    
+    for (int i=0; i<4; i++)
+    {
+        for(int j=0; j<numPlayers; j++)
+        {
+            printf("Player %d please select a square\n", j);
+            scanf("%d", &selectedSquare);
+            
+            if(board[selectedSquare][0].stack=minNumOfTokens)
+            {
+            board[selectedSquare[0].stack = (token *)malloc(sizeof(token));
+            board[selectedSquare][0].stack->col = players[j].col;
+            board[selectedSquare][0].numTokens++;
+            }
+            
+            if (((numPlayers*i)+j+1)%NUM_ROWS==0)
+                minNumOfTokens++;
+        }
+    }
 
 }
 
